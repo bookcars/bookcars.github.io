09e257cbe089f4b469c253b244841bdee50d9e2b
aelassas <akram.elassas@gmail.com>
2025-07-07 13:22:55 +0100

    fix: website title

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,7 @@ document.documentElement.setAttribute('data-loading', '')
 
       const translations = await res.json()
       document.documentElement.lang = lang
-      document.title = translations['website.title'] || 'BookCars'
+      document.title = translations['website.title']
       window.translations = translations
       window.currentLang = lang
       localStorage.setItem('lang', lang)
